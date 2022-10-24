07eb500510fd057bb6001fcf78f96f3884ef7b15
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-10-24 08:31:39 -0300

    Autenticando usuario

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,4 +1,22 @@
 <?php
-    $_POST['email'];
-    $_POST['senha'];
+    $usuario_autenticado = false;
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+    );
+
+    foreach($usuarios_app as $user){
+      
+       if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
+            $usuario_autenticado = true;
+       }
+       
+    }
+
+    if($usuario_autenticado){
+        echo 'Usuário autenticado';
+    }else{
+        header('location: index.php?login=erro');
+    }
+   
 ?>
