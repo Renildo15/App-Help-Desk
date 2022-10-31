4e70a2bed47fbdea703730226f1c99d36bf1b7e1
Renildo Rabil <renildorabi@ufrn.edu.br>
2022-10-31 08:10:44 -0300

    Controle de perfil

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -4,25 +4,36 @@
     
 
     $usuario_autenticado = false;
+    $usuario_id = null;
+    $usuario_perfil_id = null;
+
+    $perfis = array(1 => 'Administrativo', 2 => 'Usuário');
+
     $usuarios_app = array(
-        array('email' => 'adm@teste.com.br', 'senha' => '123456'),
-        array('email' => 'user@teste.com.br', 'senha' => 'abcd'),
+        array('id' => 1,'email' => 'adm@teste.com.br', 'senha' => '123456', 'perfil_id' => 1),
+        array('id' => 2,'email' => 'user@teste.com.br', 'senha' => 'abcd', 'perfil_id' => 1),
+        array('id' => 3,'email' => 'jose@teste.com.br', 'senha' => '12345','perfil_id' => 2),
+        array('id' => 4,'email' => 'maria@teste.com.br', 'senha' => '1234', 'perfil_id' => 2),
     );
 
     foreach($usuarios_app as $user){
       
        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']){
             $usuario_autenticado = true;
+            $usuario_id = $user['id'];
+            $usuario_perfil_id = $user['perfil_id'];
        }
        
     }
 
     if($usuario_autenticado){
-            $_SESSION['autenticado'] = "SIM";
-            header('Location: home.php');
+        $_SESSION['autenticado'] = "SIM";
+        $_SESSION['id'] = $usuario_id;
+        $_SESSION['perfil_id'] = $usuario_perfil_id;
+        header('Location: home.php');
     }else{
         header('location: index.php?login=erro');
-            $_SESSION['autenticado'] = "NÃO";
+        $_SESSION['autenticado'] = "NÃO";
     }
    
 ?>
